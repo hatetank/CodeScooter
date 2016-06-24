e11ccde379ea82d300471dafda33129284b51652
hatetank <gavin.e.ralston@gmail.com>
2016-06-24 10:18:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/codescooter/BusinessCard.java
@@ -0,0 +1,13 @@
+package com.example.android.codescooter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusinessCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_card);
+    }
+}
